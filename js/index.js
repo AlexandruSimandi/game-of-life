5b94c64a6eb6c32396193f64f53ef94e4ad021ad
alex simi <alex simi>
2016-08-02 00:26:23 +0300

    add pattern.js to separate data from code

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 window.onload = function(){
+//    $.getJSON('data/patterns.json', function(data){
+//        console.log(data);
+//    })
     gameModule.initialize();
     gameModule.restart();
     UIModule.initialize();
